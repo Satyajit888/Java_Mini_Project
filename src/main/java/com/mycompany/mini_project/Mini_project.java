4af84a4c4b33fafdaf678741792497ee20e4b9f6
Satyajit Pandey <satyajit.pandey@msds.christuniversity.in>
2023-05-04 11:56:11 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/mini_project/Mini_project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mini_project;
+
+/**
+ *
+ * @author pande
+ */
+public class Mini_project {
+
+    public static void main(String[] args) {
+        Signup_Page sup=new Signup_Page();
+        sup.setVisible( true);
+    }
+}
